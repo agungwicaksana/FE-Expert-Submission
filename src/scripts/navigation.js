c7ca37344e39dafaddd223eb4dc5026386df1d9d
Agung Wicaksana <agung.wicaksaana@gmail.com>
2020-08-29 10:23:05 +0700

    fix navbar

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,27 +1,31 @@
 class Nav {
     static drawer() {
+        const drawerContainer = document.querySelector('.drawer-container');
         const drawer = document.querySelector('.drawer');
         const menuBtn = document.getElementById('menu-btn');
         const closeBtn = document.querySelector('.close-btn');
         
         menuBtn.addEventListener('click', e => {
             drawer.classList.toggle('open');
-            drawer.classList.toggle('closed');
+            drawerContainer.classList.toggle('open');
             e.preventDefault();
             closeBtn.style.transform = "rotate(0deg)";
             closeBtn.style.transition = ".2s";
         })
 
-        closeBtn.addEventListener('click', e => {
+        closeBtn.addEventListener('click', e => closeDrawer(e));
+        drawerContainer.addEventListener('click', e => closeDrawer(e));
+
+        function closeDrawer(e) {
             closeBtn.style.transform = "rotate(180deg)";
             closeBtn.style.transition = ".4s";
             setTimeout(() => {
                 drawer.classList.remove('open');
-                drawer.classList.add('closed');
+                drawerContainer.classList.remove('open');
             }, 60);
             e.preventDefault();
             e.stopPropagation();
-        })
+        }
     }
 }
 
