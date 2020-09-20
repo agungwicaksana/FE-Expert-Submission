567f049196ab38d8f2019e466e776fd18b1e2ab7
Agung Wicaksana <agung.wicaksaana@gmail.com>
2020-09-20 10:45:06 +0700

    add idb

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,6 +2,9 @@ const CONFIG = {
   API_BASE_URL: 'https://dicoding-restaurant-api.el.r.appspot.com',
   AUTH_TOKEN: '12345',
   CACHE_NAME: 'MealsCorner-v1',
+  DATABASE_NAME: 'favorite-restaurant-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'restaurants',
 };
 
 export default CONFIG;
