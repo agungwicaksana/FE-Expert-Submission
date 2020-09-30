710f58c8d0fa0e10483445b92b8b78adaca3ba3e
Agung Wicaksana <agung.wicaksaana@gmail.com>
2020-09-30 15:24:15 +0700

    testing:  show unfavorite button when a restaurant has been favorited

---
+++ specs/unfavoriteRestaurantsSpec.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import FavoriteRestaurantIdb from '../src/scripts/data/idb';
+import { addFavoriteButton, initButton, restaurantTestingData } from './helper';
+
+describe('Unfavoriting a restaurant', () => {
+  beforeEach(async () => {
+    addFavoriteButton();
+    await FavoriteRestaurantIdb.putRestaurant(restaurantTestingData);
+  })
+
+  afterEach(async () => {
+    await FavoriteRestaurantIdb.deleteRestaurant(restaurantTestingData.id);
+  })
+
+  it('should display unfavorite button when restaurant has been favorited', async () => {
+    await initButton(restaurantTestingData);
+
+    expect(document.querySelector('[aria-label="Unfavorite this restaurant!"]'))
+      .toBeTruthy();
+  })
+})
