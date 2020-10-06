d020d1dc02c079bc5bfb8b4c5ac09b81bce88322
Agung Wicaksana <agung.wicaksaana@gmail.com>
2020-10-06 10:05:30 +0700

    testing: add eend to end testing

--- e2e/Favoriting_restaurant.spec.js
+++ e2e/Favoriting_restaurant.spec.js
@@ -1,5 +1,40 @@
+/* eslint-disable */
+const assert = require('assert');
+
 Feature('Favoriting restaurant');
 
-Scenario('test something', ({ I }) => {
+Before(({ I }) => {
+  I.amOnPage('/#/favorite');
+})
 
+Scenario('Favoriting a restaurant then unfavorite it', async ({ I }) => {
+  // Favoriting a restaurant
+  I.seeElement('#restaurants-section');
+  I.see('No Favorite Restaurants', 'h2');
+
+  I.amOnPage('/');
+  
+  I.seeElement('.card a');
+  I.click(locate('.card a').first());
+  const firstRestaurant = locate('.card a .card-title').first();
+  const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
+
+
+  I.seeElement('.favorite-button');
+  I.click('.favorite-button');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.card');
+  const favoritedRestaurantName = await I.grabTextFrom('.card-title');
+
+  assert.strictEqual(firstRestaurantName, favoritedRestaurantName);
+
+  // Unfavorite it
+  I.click('.card a');
+
+  I.seeElement('.favorite-button');
+  I.click('.favorite-button');
+
+  I.amOnPage('/#/favorite');
+  I.see('No Favorite Restaurants', 'h2');
 });
